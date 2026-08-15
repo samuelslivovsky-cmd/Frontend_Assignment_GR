c1983c1556c73a4a54e41e50678fded1d38f1b31
Samuel Slivovsky <samuelslivovsky@gmail.com>
2026-08-15 10:04:09 +0200

    fix: removed unnecessary skip button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,22 +37,10 @@ export default function RootLayout({ children }: LayoutProps<'/'>) {
     <html lang={DEFAULT_LOCALE} className={`${inter.variable} h-full antialiased`}>
       <body className="flex min-h-full flex-col bg-white text-gray-900">
         <Providers>
-          <SkipLink />
           {children}
           <Notifications />
         </Providers>
       </body>
     </html>
-  )
-}
-
-function SkipLink() {
-  return (
-    <a
-      href="#obsah"
-      className="sr-only focus:not-sr-only focus:absolute focus:top-4 focus:left-4 focus:z-50 focus:rounded-lg focus:bg-indigo-600 focus:px-4 focus:py-2 focus:text-sm focus:text-white"
-    >
-      {serverT('common.skipToContent')}
-    </a>
   )
 }
